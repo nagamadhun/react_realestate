bb8e7fac09569fd8ae04888ea8a8a8f48d116915
nagamadhun <nadakudurunagamadhu@gmail.com>
2024-03-08 18:06:53 -0500

    flex in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,13 +4,16 @@ export default function Navbar() {
   return (
     <nav>
         <div className='left'>
-          <a href='/'>
-            <img src='/logo.png' alt='logo' />
+          <a href='/' className='logo'>
+            <img src='/logo.png'  alt='logo' />
             <span>ReactRealEstate</span>
           </a>
           <a href="/">Home</a><a href="/">About</a><a href="/">Contact</a><a href="/">Agents</a>
         </div>
-        <div className='right'>Right</div>
+        <div className='right'>
+          <a href='/'>Sign In</a>
+          <a href='/' className='register'>Sign Up</a>
+        </div>
     </nav>
   )
 }
