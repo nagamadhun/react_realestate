743bcc9f5b5c64435647ce2b52bf9eb071caeb3a
nagamadhun <nadakudurunagamadhu@gmail.com>
2024-03-09 16:51:52 -0500

    added background image in the homescreen right div

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './layout.scss'
 import Navbar from './components/navbar/Navbar'
+import { HomePage } from './routes/homePage/HomePage'
 
 export default function App() {
   return (
     <div className='layout'>
+      <div className="navbar">
       <Navbar />
+
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
     </div>
   )
 }
---
+++ src/routes/homePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './homePage.scss'
+export const HomePage = () => {
+  return (
+    <div className='homePage'>
+        <div className="textContainer">text</div>
+        <div className="imgContainer">
+            <img src="/bg.png" alt="bg" />
+        </div>
+    </div>
+  )
+}
