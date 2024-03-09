6c76586558430e961a0e10dd16e2b259b342ff95
nagamadhun <nadakudurunagamadhu@gmail.com>
2024-03-09 16:03:24 -0500

    added code to make app responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,6 +13,15 @@ export default function Navbar() {
         <div className='right'>
           <a href='/'>Sign In</a>
           <a href='/' className='register'>Sign Up</a>
+          <div className='menuIcon'>
+            <img src='/menu.png' alt='menu' />
+          </div>
+          <div className="menu">
+          <a href="/">Home</a><a href="/">About</a><a href="/">Contact</a><a href="/">Agents</a>
+          <a href='/'>Sign In</a>
+          <a href='/'>Sign Up</a>
+
+          </div>
         </div>
     </nav>
   )
