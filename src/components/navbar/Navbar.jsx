dd0bc20717212d7c440d5a1ca6c0114d3981009c
nagamadhun <nadakudurunagamadhu@gmail.com>
2024-03-09 16:14:03 -0500

    added onclick for menu bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import './navbar.scss'
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false)
   return (
     <nav>
         <div className='left'>
@@ -14,9 +16,9 @@ export default function Navbar() {
           <a href='/'>Sign In</a>
           <a href='/' className='register'>Sign Up</a>
           <div className='menuIcon'>
-            <img src='/menu.png' alt='menu' />
+            <img src='/menu.png' alt='menu' onClick={() => setOpen(prev => !prev)} />
           </div>
-          <div className="menu">
+          <div className={open ? "menu active" : "menu"}>
           <a href="/">Home</a><a href="/">About</a><a href="/">Contact</a><a href="/">Agents</a>
           <a href='/'>Sign In</a>
           <a href='/'>Sign Up</a>
